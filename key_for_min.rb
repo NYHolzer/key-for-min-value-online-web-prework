0e5638e2524d2277b0f6c3cc441795241816eadf
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-20 19:29:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     nil 
   else
     name_hash.collect do |key, value|
-      lowest = name_hash["key"]
+      lowest_value = name_hash["key"]
       key_min = ""
       if value < lowest
         lowest = value
