fa173f9a68fb73f207791b95e7d048a6c495b151
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-20 19:29:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0 
+    nil 
+  else
+    name_hash.collect do |key, value|
+      lowest = name_hash["key"]
+      key_min = ""
+      if value < lowest
+        lowest = value
+        key_min = key
+      end
+    key_min
+    end
+  end
 end
